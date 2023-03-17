a9a80166348ab591dbfd53f41439bfd04b9f2ed1
eminsin <116303962+eminsin@users.noreply.github.com>
2023-03-17 20:02:53 +0100

    Update Covid2023.sql

--- Covid2023.sql
+++ Covid2023.sql
@@ -2,7 +2,7 @@
 
 /*******************************************************************************************************************
 
-Covid 2023 Data Exploration 
+Covid-19 Data Exploration 
 Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
 
 ********************************************************************************************************************/
